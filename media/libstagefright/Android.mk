66a45ae255e6bb9817e6e34a05d8c46c615d9788
draken1994 <dwkryadav25@gmail.com>
2022-03-17 13:34:42 +0530

    revert Fix camera color tibet

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -147,10 +147,6 @@ LOCAL_SHARED_LIBRARIES += \
         libdl \
         libRScpp \
 
-ifeq ($(MTK_HARDWARE), true)
-LOCAL_CFLAGS += -DMTK_HARDWARE
-endif
-
 ifeq ($(BOARD_USE_SAMSUNG_CAMERAFORMAT_NV21), true)
 # This needs flag requires the following string constant in
 # CameraParametersExtra.h:
