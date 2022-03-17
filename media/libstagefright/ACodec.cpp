5c5d938b47b1e2134ef79cdaf2aa8454a9343f5c
draken1994 <dwkryadav25@gmail.com>
2022-03-17 16:36:10 +0530

    colour

--- media/libstagefright/ACodec.cpp
+++ media/libstagefright/ACodec.cpp
@@ -85,16 +85,6 @@
 
 #include <stagefright/AVExtensions.h>
 
-#ifdef MTK_HARDWARE
-#include <media/stagefright/dpframework/DpBlitStream.h>
-
-#define HAL_PIXEL_FORMAT_NV12_BLK 0x7F000001
-#define HAL_PIXEL_FORMAT_I420 (0x32315659 + 0x10)
-
-const OMX_COLOR_FORMATTYPE OMX_MTK_COLOR_FormatYV12 = (OMX_COLOR_FORMATTYPE)0x7F000200;
-const OMX_COLOR_FORMATTYPE OMX_COLOR_FormatVendorMTKYUV = (OMX_COLOR_FORMATTYPE)0x7F000001;
-#endif
-
 namespace android {
 
 enum {
@@ -1046,20 +1036,13 @@ status_t ACodec::setupNativeWindowSizeFormatAndUsage(
     setNativeWindowColorFormat(eNativeColorFormat);
 #endif
 
-#ifdef MTK_HARDWARE
-    OMX_COLOR_FORMATTYPE eHalColorFormat = def.format.video.eColorFormat;
-    setHalWindowColorFormat(eHalColorFormat);
-#endif
-
     ALOGV("gralloc usage: %#x(OMX) => %#x(ACodec)", omxUsage, usage);
     err = setNativeWindowSizeFormatAndUsage(
             nativeWindow,
             def.format.video.nFrameWidth,
             def.format.video.nFrameHeight,
 #ifdef USE_SAMSUNG_COLORFORMAT
-            eNativeColorFormat,
-#elif MTK_HARDWARE
-            eHalColorFormat			
+            eNativeColorFormat,		
 #else
             def.format.video.eColorFormat,
 #endif
@@ -1450,30 +1433,6 @@ void ACodec::setNativeWindowColorFormat(OMX_COLOR_FORMATTYPE &eNativeColorFormat
 }
 #endif
 
-#ifdef MTK_HARDWARE
-void ACodec::setHalWindowColorFormat(OMX_COLOR_FORMATTYPE &eHalColorFormat) {
-    ALOGE("[Decker] setHalWindowColorFormat(%#x) - %s",eHalColorFormat,mComponentName.c_str());
-
-    if (!strncmp("OMX.MTK.", mComponentName.c_str(), 8)) {
-        switch (eHalColorFormat) {
-            case OMX_COLOR_FormatYUV420Planar:
-                eHalColorFormat = (OMX_COLOR_FORMATTYPE)HAL_PIXEL_FORMAT_I420;
-                break;
-            case OMX_MTK_COLOR_FormatYV12:
-                eHalColorFormat = (OMX_COLOR_FORMATTYPE)HAL_PIXEL_FORMAT_YV12;
-                break;
-            case OMX_COLOR_FormatVendorMTKYUV:
-                eHalColorFormat = (OMX_COLOR_FORMATTYPE)HAL_PIXEL_FORMAT_NV12_BLK;
-                break;
-            default:
-                eHalColorFormat = (OMX_COLOR_FORMATTYPE)HAL_PIXEL_FORMAT_I420;
-                break;
-        }
-    }
-}
-#endif
-
->>>>>>> e8e6db501... Add MediaTek color format support
 status_t ACodec::cancelBufferToNativeWindow(BufferInfo *info) {
     CHECK_EQ((int)info->mStatus, (int)BufferInfo::OWNED_BY_US);
 
